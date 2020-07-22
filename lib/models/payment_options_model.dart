071af53f166e11bc8651c40e3edb0b8d5b286674
Tinotenda Joel Muchenje <tinotendam@ajs.co.za>
2020-07-23 00:34:18 +0200

    Refactoring and placeholder images

--- lib/models/payment_options_model.dart
+++ lib/models/payment_options_model.dart
@@ -9,27 +9,27 @@ class PaymentOptions{
 
 final paymentOptions = [
   PaymentOptions(
-    image: Image.asset('assets/zinwa_medium.png'),
+    image: Image.asset('assets/ecocash.jpg'),
     title: 'Ecocash',
   ),
   PaymentOptions(
-    image: Image.asset('assets/zinwa_medium.png'),
+    image: Image.asset('assets/onemoney.png'),
     title: 'One Money',
   ),
   PaymentOptions(
-    image: Image.asset('assets/zinwa_medium.png'),
+    image: Image.asset('assets/telecash.png'),
     title: 'Telecash',
   ),
   PaymentOptions(
-    image: Image.asset('assets/zinwa_medium.png'),
+    image: Image.asset('assets/bank.png'),
     title: 'Bank EFT',
   ),
   PaymentOptions(
-    image: Image.asset('assets/zinwa_medium.png'),
+    image: Image.asset('assets/bank.png'),
     title: 'Nostro',
   ),
   PaymentOptions(
-    image: Image.asset('assets/zinwa_medium.png'),
+    image: Image.asset('assets/visamaster.png'),
     title: 'Visa/MasterCard',
   ),
 ];
--- lib/views/payment_options.dart
+++ lib/views/payment_options.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:zinwa_pay/models/payment_options_model.dart';
+import 'package:zinwa_pay/views/payment.dart';
 
 class PaymentOptionsScreen extends StatefulWidget {
   @override
@@ -12,7 +13,6 @@ class _State extends State<PaymentOptionsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFFAFAFA),
       appBar: AppBar(
         title: Text('Payment Methods'),
         centerTitle: true,
@@ -26,7 +26,7 @@ class _State extends State<PaymentOptionsScreen> {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            onPressed: () => print('HELP'),
+            onPressed: () => print(''),
           )
         ],
       ),
@@ -42,37 +42,19 @@ class _State extends State<PaymentOptionsScreen> {
             alignment: Alignment.center,
             margin: EdgeInsets.all(10.0),
             width: double.infinity,
-            height: 80.0,
+            height: 100.0,
             decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.circular(10.0),
-              border: _selectedOption == index - 1
-                  ? Border.all(color: Colors.blue)
-                  : null,
+              border: _selectedOption == index - 1 ? Border.all(color: Colors.blue) : null,
             ),
-            child: ListTile(
-              leading: paymentOptions[index - 1].image,
-              title: Text(
-                paymentOptions[index - 1].title,
-                style: TextStyle(
-                  color: _selectedOption == index - 1
-                      ? Colors.blue
-                      : Colors.grey[600],
-                ),
-              ),
-              selected: _selectedOption == index - 1,
-              onTap: () {
-                setState(() {
-                  _selectedOption = index - 1;
-                });
-              },
-            ),
+            child: buildListTile(index, context),
           );
         },
       ),
       bottomSheet: Container(
         width: double.infinity,
-        height: 80.0,
+        height: 20.0,
         color: Color(0xFFF3F3F3),
         child: Padding(
           padding: EdgeInsets.only(right: 20.0),
@@ -81,7 +63,7 @@ class _State extends State<PaymentOptionsScreen> {
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
               Text(
-                'SAVE & CONTINUE',
+                '',
                 style: TextStyle(
                   color: Colors.black,
                   fontSize: 18.0,
@@ -91,12 +73,36 @@ class _State extends State<PaymentOptionsScreen> {
               Icon(
                 Icons.arrow_forward_ios,
                 color: Colors.black,
-                size: 18.0,
+                size: 2.0,
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  ListTile buildListTile(int index, BuildContext context) {
+    return ListTile(
+      leading: paymentOptions[index - 1].image,
+      title: Text(
+        paymentOptions[index - 1].title,
+        style: TextStyle(
+          color: _selectedOption == index - 1 ? Colors.blue : Colors.grey[600],
+        ),
+      ),
+      selected: _selectedOption == index - 1,
+      onTap: () {
+        setState(() {
+          _selectedOption = index - 1;
+        });
+        if (paymentOptions[index - 1].title == "Ecocash") {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Payment()),
+          );
+        }
+      },
     );
   }
 }
